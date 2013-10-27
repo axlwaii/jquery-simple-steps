60fe518d9f34f89da7c357a1053ab91aa1fc66c8
axlwaii <mwfort-github@yahoo.com>
2013-10-27 18:20:40 +0100

    change indetion to 4 spaces

--- src/jquery-simple-steps.js
+++ src/jquery-simple-steps.js
@@ -1,105 +1,101 @@
 /* Version 0.1.1 */
 (function($){
-  $.fn.simpleSteps = function (options){
-    options = $.extend({
-      stepClass           : '.step',
-      animationSpeed      : 500
-    }, options);
+    $.fn.simpleSteps = function (options){
+        options = $.extend({
+            stepClass           : '.step',
+            animationSpeed      : 500
+        }, options);
 
-    var stepsContainer = this;
+        var stepsContainer = this,
+            currentStep = 0,
+            $nextButtons = $('.next-step-button'),
+            steps, validations;
 
-    var currentStep, nextButtons, steps, validations;
+         function validateEmail(element) {
 
-    currentStep = 0;
+            var i,re;
+            var isValid = true;
+            var elements = $(element).find('.validate.email');
+            if(elements.length > 0){
+                // from http://stackoverflow.com/a/46181/11236
+                re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+                for(i=0; i < elements.length; i++){
+                    $(elements[i]).removeClass('error');
+                    if(!re.test(elements[i].value)){
+                        isValid = false;
+                        $(elements[i]).addClass('error');
+                    }
+                }
+            }
 
-    validations = {
-      "presence": validatePresence,
-      "email"   : validateEmail
+            return isValid;
+        }
+
+        function validatePresence(element){
+            var i;
+            var elements = $(element).find('.validate.presence');
+            var isValid = true;
+            if(elements.length > 0){
+                for(i=0; i < elements.length; i++){
+                    $(elements[i]).removeClass('error');
+                    if(elements[i].value === ""){
+                        isValid = false;
+                        $(elements[i]).addClass('error');
+                    }
+                }
+            }
+            return isValid;
+        }
+
+        validations = {
+            "presence": validatePresence,
+            "email"   : validateEmail
+        };
+
+        function hideSteps() {
+            var i;
+            for(i=0; i < steps.length; i++){
+                if( i !== currentStep){
+                    $(steps[i]).hide();
+                }
+            }
+        }
+
+        function init(){
+            steps = $(stepsContainer).find(options.stepClass);
+            hideSteps();
+            $(steps).first().show();
+        }
+
+        function nextStep(dir){
+            $(steps[currentStep]).hide();
+            currentStep += dir;
+            $(steps[currentStep]).fadeIn(options.animationSpeed);
+        }
+
+        $nextButtons.on('click', function(){
+            var key, allValid;
+            allValid = true;
+            for(key in validations){
+                if(!validations[key](steps[currentStep])){
+                    allValid = false;
+                }
+            }
+
+            if(allValid){
+                nextStep(1);
+            }
+
+            return false;
+        });
+
+        $('.prev-step-button').on('click',function(){
+            nextStep(-1);
+            return false;
+        });
+
+        init();
+
+        return stepsContainer;
     };
-
-    nextButtons = $('.next-step-button');
-
-    function init(){
-      steps = $(stepsContainer).find(options.stepClass);
-      hideSteps();
-      $(steps).first().show();
-    }
-
-    function nextStep(dir){
-      $(steps[currentStep]).hide();
-      currentStep += dir;
-      $(steps[currentStep]).fadeIn(options.animationSpeed);
-    }
-
-    function hideSteps() {
-      var i;
-      for(i=0; i < steps.length; i++){
-        if( i !== currentStep){
-          $(steps[i]).hide();
-        }
-      }
-    }
-
-    function validateEmail(element) {
-
-      var i,re;
-      var isValid = true;
-      var elements = $(element).find('.validate.email');
-      if(elements.length > 0){
-        // from http://stackoverflow.com/a/46181/11236
-        re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-        for(i=0; i < elements.length; i++){
-          $(elements[i]).removeClass('error');
-          if(!re.test(elements[i].value)){
-            isValid = false;
-            $(elements[i]).addClass('error');
-          }
-        }
-      }
-
-      return isValid;
-    }
-
-
-    function validatePresence(element){
-      var i;
-      var elements = $(element).find('.validate.presence');
-      var isValid = true;
-      if(elements.length > 0){
-        for(i=0; i < elements.length; i++){
-          $(elements[i]).removeClass('error');
-          if(elements[i].value === ""){
-            isValid = false;
-            $(elements[i]).addClass('error');
-          }
-        }
-      }
-      return isValid;
-    }
-
-    $('.next-step-button').on('click', function(e){
-      var key, allValid;
-      allValid = true;
-      for(key in validations){
-        if(!validations[key](steps[currentStep])){
-          allValid = false;
-        }
-      }
-
-      if(allValid){
-        nextStep(1);
-      }
-
-      return false;
-    });
-
-    $('.prev-step-button').on('click',function(){
-      nextStep(-1);
-      return false;
-    });
-
-    init();
-
-    return stepsContainer;
-  };
 }(jQuery));
--- test/jquery-simple-steps-spec.js
+++ test/jquery-simple-steps-spec.js
@@ -12,31 +12,31 @@ var $ = global.jQuery = jQuery.create(window);
 require("../src/jquery-simple-steps");
 
 describe('simple steps on init', function(){
-  before(function(){
-    $('#simple-step').simpleSteps();
-  });
+    before(function(){
+        $('#simple-step').simpleSteps();
+    });
 
-  it('should show first step', function(){
-    expect($('.step').first().attr('style')).to.not.include('display: none');
-  });
+    it('should show first step', function(){
+        expect($('.step').first().attr('style')).to.not.include('display: none');
+    });
 
-  it('should hide other steps', function() {
-    expect($('.step').first().next().attr('style')).to.include('display: none');
-  });
+    it('should hide other steps', function() {
+        expect($('.step').first().next().attr('style')).to.include('display: none');
+    });
 
 });
 
 describe('simple steps on next-step-button click', function(){
-  before(function(){
-    $('.next-step-button').trigger('click');
-  });
+    before(function(){
+        $('.next-step-button').trigger('click');
+    });
 
-  it('should hide the current step', function(){
-    expect($('.step').first().attr('style')).to.include('display: none');
-  });
+    it('should hide the current step', function(){
+        expect($('.step').first().attr('style')).to.include('display: none');
+    });
 
-  it('should show the  next step', function(){
-    expect($('.step').first().next().attr('style')).to.not.include('display: none');
-  });
+    it('should show the  next step', function(){
+        expect($('.step').first().next().attr('style')).to.not.include('display: none');
+    });
 
 });
